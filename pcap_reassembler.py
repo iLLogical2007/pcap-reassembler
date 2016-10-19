35160da546dff033d8f7c8a6d2bdcbff2d2575b4
iLLogical2007 <tobias@hannaske.eu>
2016-10-19 08:33:19 +0200

    Python 3 support

--- pcap_reassembler.py
+++ pcap_reassembler.py
@@ -6,33 +6,36 @@ packets found in a pcap file.
 """
 
 import sys
-import pcap
+import pcapy
 import struct
 import time
 
 # OSI layer constants
-PHYSICAL_LAYER      = 1
-DATA_LINK_LAYER     = 2
-NETWORK_LAYER       = 3
-TRANSPORT_LAYER     = 4
-SESSION_LAYER       = 5
-PRESENTATION_LAYER  = 6
-APPLICATION_LAYER   = 7
+from binascii import hexlify
+
+PHYSICAL_LAYER = 1
+DATA_LINK_LAYER = 2
+NETWORK_LAYER = 3
+TRANSPORT_LAYER = 4
+SESSION_LAYER = 5
+PRESENTATION_LAYER = 6
+APPLICATION_LAYER = 7
 
 # TPID from IEEE 802.1Q
-_tpid = '\x81\x00'
+_tpid = b'\x81\x00'
 
 # EtherType constants
 _ether_type = {
-    'IPv4': '\x08\x00',
-    'IPv6': '\x86\xdd',
+    'IPv4': b'\x08\x00',
+    'IPv6': b'\x86\xdd',
 }
 
 # IP protocol field constants
 _ip_protocol = {
-    'TCP': '\x06',
-    'UDP': '\x11',
+    'TCP': b'\x06',
+    'UDP': b'\x11',
 }
+
 
 class Message(dict):
     """Reassembled message class
@@ -49,6 +52,7 @@ class Message(dict):
      * payload      - the raw byte data of the message payload
 
     """
+
     @property
     def ts(self):
         return self.fragment_tss[0]
@@ -56,18 +60,19 @@ class Message(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
 
+
 class PcapReassembler:
     def __init__(self):
         # TCP stream buffer
-        self._tcp_stream     = None
+        self._tcp_stream = None
         # message buffer
-        self._msgs           = None
+        self._msgs = None
         # packet count
-        self._count          = 1
+        self._count = 1
         # OSI layer
-        self._layer          = 4
+        self._layer = 4
         # strict TCP reassembly policy
-        self._strict_policy  = False
+        self._strict_policy = False
 
     def load_pcap(self, filename, layer=TRANSPORT_LAYER, strict=False):
         """Loads a pcap file and returns a list of Message objects
@@ -84,18 +89,18 @@ class PcapReassembler:
         """
         if not DATA_LINK_LAYER <= layer <= TRANSPORT_LAYER:
             raise ValueError("Specified OSI layer is not supported.")
-        self._tcp_stream    = {}
-        self._msgs          = []
-        self._count         = 1
-        self._layer         = layer
+        self._tcp_stream = {}
+        self._msgs = []
+        self._count = 1
+        self._layer = layer
         self._strict_policy = strict
-        p = pcap.pcapObject()
-        p.open_offline(filename)
+
+        p = pcapy.open_offline(filename)
         # process all packets
         try:
-            p.dispatch(-1, self._process_eth)
+            p.loop(0, self._process_eth)
         except Exception as e:
-            print e
+            print(e)
 
         # flush all TCP connections for remaining messages
         for socks in self._tcp_stream:
@@ -103,12 +108,14 @@ class PcapReassembler:
         self._msgs.sort(key=lambda x: x.number)
         return self._msgs
 
-    def _process_eth(self, length, data, ts):
+    def _process_eth(self, header, data):
         """Processes an Ethernet packet (header to checksum; not the full frame).
 
         May propagate processing to the correct IP version processing function.
 
         """
+        ts = header.getts()[0]
+
         dst_addr = data[0:6]
         src_addr = data[6:12]
         ieee_8021q = data[12:14] == _tpid
@@ -126,13 +133,13 @@ class PcapReassembler:
                 pass
         else:
             msg = Message({
-                'number':           self._count,
-                'fragment_tss':     [ts],
-                'data':             ''.join(data),
-                'src_addr':         src_addr,
-                'dst_addr':         dst_addr,
-                'eth_type':         eth_type,
-                'payload':          ''.join(pld),
+                'number': self._count,
+                'fragment_tss': [ts],
+                'data': ''.join(data),
+                'src_addr': src_addr,
+                'dst_addr': dst_addr,
+                'eth_type': eth_type,
+                'payload': ''.join(pld),
             })
             if ieee_8021q:
                 msg['tci'] = tci
@@ -147,9 +154,9 @@ class PcapReassembler:
         function.
 
         """
-        header_len = 4 * (_decode_byte(data[0]) & 0x0f)
+        header_len = 4 * (_decode_byte(data[0:1]) & 0x0f)
         tot_len = _decode_short(data[2:4])
-        ip_type = data[9]
+        ip_type = data[9:10]
         src = data[12:16]
         dst = data[16:20]
         pld = data[header_len:tot_len]
@@ -162,13 +169,13 @@ class PcapReassembler:
                 pass
         else:
             msg = Message({
-                'number':           self._count,
-                'fragment_tss':     [ts],
-                'data':             ''.join(data[:tot_len]),
-                'ip_type':          ip_type,
-                'src_addr':         src,
-                'dst_addr':         dst,
-                'payload':          ''.join(pld),
+                'number': self._count,
+                'fragment_tss': [ts],
+                'data': ''.join(data[:tot_len]),
+                'ip_type': ip_type,
+                'src_addr': src,
+                'dst_addr': dst,
+                'payload': ''.join(pld),
             })
             self._msgs.append(msg)
 
@@ -188,36 +195,36 @@ class PcapReassembler:
         dst_port = _decode_short(data[2:4])
         seq = _decode_word(data[4:8])
         ack = _decode_word(data[8:12])
-        offset = (_decode_byte(data[12]) & 0xf0) >> 4
-        pld = data[4*offset:]
-        src_socket  = (src_addr, src_port)
-        dst_socket  = (dst_addr, dst_port)
-        sockets     = (src_socket, dst_socket)
+        offset = (_decode_byte(data[12:13]) & 0xf0) >> 4
+        pld = data[4 * offset:]
+        src_socket = (src_addr, src_port)
+        dst_socket = (dst_addr, dst_port)
+        sockets = (src_socket, dst_socket)
         if pld:
             if not sockets in self._tcp_stream:
                 self._tcp_stream[sockets] = Message({
-                    'number':           self._count,
-                    'fragment_tss':     [],
-                    'data':             [],
-                    'ip_proto':         'TCP',
-                    'src_addr':         src_addr,
-                    'dst_addr':         dst_addr,
-                    'src_port':         src_port,
-                    'dst_port':         dst_port,
-                    'seq':              seq,
-                    'ack':              ack,
-                    'payload':          [],
+                    'number': self._count,
+                    'fragment_tss': [],
+                    'data': [],
+                    'ip_proto': 'TCP',
+                    'src_addr': src_addr,
+                    'dst_addr': dst_addr,
+                    'src_port': src_port,
+                    'dst_port': dst_port,
+                    'seq': seq,
+                    'ack': ack,
+                    'payload': [],
                 })
             msg = self._tcp_stream[sockets]
             msg.fragment_tss.append(ts)
-            msg.data.append(''.join(data))
+            #msg.data.append(''.join(data))
             offset = seq - msg.seq
-            msg.payload[offset:offset+len(pld)] = list(pld)
+            msg.payload[offset:offset + len(pld)] = list(pld)
         if self._strict_policy:
             # Check the other stream in the connection
             sockets = sockets[::-1]
             if (sockets in self._tcp_stream and ack == self._tcp_stream[sockets].seq +
-                    len(self._tcp_stream[sockets].payload)):
+                len(self._tcp_stream[sockets].payload)):
                 self._tcp_flush(sockets)
                 del self._tcp_stream[sockets]
         else:
@@ -232,7 +239,6 @@ class PcapReassembler:
 
         """
         msg = self._tcp_stream[sockets]
-        msg.payload = ''.join(msg.payload)
         self._msgs.append(msg)
 
     def _process_udp(self, ts, src_addr, dst_addr, data):
@@ -245,29 +251,33 @@ class PcapReassembler:
         src_port = _decode_short(data[0:2])
         dst_port = _decode_short(data[2:4])
         msg = Message({
-            'number':           self._count,
-            'fragment_tss':     [ts],
-            'data':             ''.join(data),
-            'ip_proto':         'UDP',
-            'src_addr':         src_addr,
-            'dst_addr':         dst_addr,
-            'src_port':         src_port,
-            'dst_port':         dst_port,
-            'payload':          ''.join(data[8:]),
+            'number': self._count,
+            'fragment_tss': [ts],
+            'data': ''.join(data),
+            'ip_proto': 'UDP',
+            'src_addr': src_addr,
+            'dst_addr': dst_addr,
+            'src_port': src_port,
+            'dst_port': dst_port,
+            'payload': ''.join(data[8:]),
         })
         self._msgs.append(msg)
+
 
 def _decode_byte(data):
     """Decodes one byte of network data into an unsigned char."""
     return struct.unpack('!B', data)[0]
 
+
 def _decode_short(data):
     """Decodes two bytes of network data into an unsigned short."""
     return struct.unpack('!H', data)[0]
 
+
 def _decode_word(data):
     """Decodes four bytes of network data into an unsigned int."""
     return struct.unpack('!I', data)[0]
+
 
 def validate_tcp_checksum(length, src, dst, data):
     """Validates a TCP checksum according to RFC 1071.
@@ -292,10 +302,11 @@ def validate_tcp_checksum(length, src, dst, data):
     sum += 0x0006
     sum += length
     for i in range(0, len(data), 2):
-        sum += _decode_short(data[i] + data[i+1])
+        sum += _decode_short(data[i] + data[i + 1])
     while sum >> 16:
         sum = (sum & 0xffff) + (sum >> 16)
     return ~sum & 0xffff == csum
+
 
 def address_to_string(b):
     """Converts an IP address to its string representation.
@@ -307,3 +318,14 @@ def address_to_string(b):
     assert len(b) == 4
     b = map(lambda x: str(_decode_byte(x)), b)
     return '.'.join(b)
+
+
+if __name__ == '__main__':
+    reassembler = PcapReassembler()
+    msgs = reassembler.load_pcap('test.pcap')
+
+    for m in msgs:
+        output = bytes(m.payload)
+        print(output)
+
+    print("ok")
